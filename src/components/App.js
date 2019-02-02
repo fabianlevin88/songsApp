ed4f81a9cdcc930bfcd6ff5be5e9f486ab30c54a
fabitolevin88 <fabianlevin60@gmail.com>
2019-02-02 18:51:32 -0300

    Basic structure

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = props => {
+    return (
+        <div className="ui container">
+            <div className="ui segment">
+                <h1 class="ui header">Songs</h1>
+            </div>
+        </div>
+    )
+}
+
+export default App;
